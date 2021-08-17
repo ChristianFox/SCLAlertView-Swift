c7a494c3fa356b5ec68bccdc7425d8ec00b4d6c3
Christian Fox <christianfox@kfxtech.com>
2021-08-17 19:44:34 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package =   Package(
   name: "SCLAlertView",
-  platforms: [.iOS(.v8)],
+  platforms: [.iOS(.v11)],
   products: [
     .library(name: "SCLAlertView", targets: ["SCLAlertView"]),
   ],
